8827c9e8a5c10c7654591a3e7ff43377909b03ae
SAILAJA SENAPATI <senapatisailaja.03@gmail.com>
2023-09-21 11:36:09 +0530

    Intial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,69 @@
+import { Box, HStack, Heading, Stack, Input, Button, VStack, Text } from '@chakra-ui/react'
+import React from 'react'
+import { AiOutlineSend } from 'react-icons/ai';
+
+const Footer = () => {
+  return <Box bgColor={'blackAlpha.900'} minH={"40"} p="16" color="white">
+    <Stack direction={['column', 'row']}>
+    <VStack alignItems={'stretch'} w={'full'} px={'4'}>
+        <Heading size="md"
+            textTransform={'uppercase'}
+            textAlign={['center', 'left']}>
+            Subscribe to get updates    
+        </Heading>
+        <HStack borderBottom={'2px solid white'} py="2">
+            <Input placeholder="Enter Email Here..."
+              border={'none'}
+              borderRadius="none"
+              outline={'none'}
+              focusBorderColor="none"
+            />
+
+            <Button p={'0'}
+              colorScheme={'purple'}
+              variant={'ghost'}
+              borderRadius={'0 20px 20px 0'}
+            >
+            <AiOutlineSend size={20} />
+            </Button>
+
+        </HStack>
+        </VStack>
+
+        <VStack w={'full'}
+          borderLeft={['none', '1px solid white']}
+          borderRight={['none', '1px solid white']}
+        >
+          <Heading textTransform={'uppercase'} textAlign={'center'}>
+            VIDEO HUB
+          </Heading>
+          <Text>All rights reserved</Text>
+        </VStack>
+
+        <VStack w={'full'}>
+          <Heading size={'md'} textTransform={'uppercase'}>
+            Social Media
+          </Heading>
+          <Button variant={'link'} colorScheme={'white'}>
+            <a target={'blank'} href="https://www.linkedin.com/in/sailaja-senapati/">
+              Linkedlin
+            </a>
+          </Button>
+          <Button variant={'link'} colorScheme={'white'}>
+            <a target={'blank'} href="https://www.instagram.com/sailaja_senapati/">
+              Instagram
+            </a>
+          </Button>
+
+          <Button variant={'link'} colorScheme={'white'}>
+            <a target={'blank'} href="https://github.com/sailaja22">
+              Github
+            </a>
+          </Button>
+
+        </VStack>
+    </Stack>
+  </Box>
+}
+
+export default Footer
